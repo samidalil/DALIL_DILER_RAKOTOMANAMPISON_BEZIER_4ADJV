87b841718db666631e863882bc052155884168c2
Tom <t.rakoto76@gmail.com>
2021-05-14 20:43:36 +0200

    fix bugs

--- Assets/Scripts/Pixel Rendering/MouseController.cs
+++ Assets/Scripts/Pixel Rendering/MouseController.cs
@@ -8,13 +8,13 @@ using UnityEngine.UI;
 
 public enum Mode
 {
-    NONE = 0 ,
-    EDIT = 1 ,
+    NONE = 0,
+    EDIT = 1,
     CREATION = 2
 };
+
 public class MouseController : MonoBehaviour
 {
-
     public Camera cam;
     public GameObject bezierMenu;
     public Mode mode;
@@ -45,15 +45,15 @@ public class MouseController : MonoBehaviour
 
     private Vector3 mousePoint;
     private bool editMode = false;
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 position = Input.mousePosition;
-            
-            if (mode==Mode.NONE)
-            mousePoint = Input.mousePosition;
-            mousePoint.z = zIndex;
+
+            if (mode == Mode.NONE)
+                mousePoint = Input.mousePosition;
             // Check if the mouse was clicked over a UI element
             if (!EventSystem.current.IsPointerOverGameObject())
             {
@@ -65,20 +65,12 @@ public class MouseController : MonoBehaviour
                 // Check if the mouse was clicked over a UI element
                 if (!EventSystem.current.IsPointerOverGameObject())
                 {
-                    
-                if (!editMode)
-                {
-                    Vector3 position = Input.mousePosition;
-
-                    Instantiate(testSphere, cam.ScreenToWorldPoint(position), Quaternion.identity);
-                }
-                    position.z = zIndex;
-                    Instantiate(pointPrefab, Camera.main.ScreenToWorldPoint(position), Quaternion.identity);
+                    if (!editMode)
+                    {
+                        Instantiate(testSphere, cam.ScreenToWorldPoint(position), Quaternion.identity);
+                    }
                 }
             }
-            
         }
-        
     }
 }
-

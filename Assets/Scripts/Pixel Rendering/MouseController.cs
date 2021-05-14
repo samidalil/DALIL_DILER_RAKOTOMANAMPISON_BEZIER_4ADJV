c7e1a5ec5356bd4f865201facd57a1d320e370c5
Tom <t.rakoto76@gmail.com>
2021-05-14 20:41:19 +0200

    menu bezier

--- Assets/Scripts/Pixel Rendering/MouseController.cs
+++ Assets/Scripts/Pixel Rendering/MouseController.cs
@@ -1,35 +1,69 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
+public enum Mode
+{
+    NONE = 0 ,
+    EDIT = 1 ,
+    CREATION = 2
+};
 public class MouseController : MonoBehaviour
 {
 
     public Camera cam;
-
-
+    public GameObject bezierMenu;
+    public Mode mode;
     public GameObject testSphere;
     private bool _click;
 
-    public float zIndex;
+    public void Start()
+    {
+        switchToNoneMode();
+    }
 
-    
 
+    public void switchToEditMode()
+    {
+        mode = Mode.EDIT;
+    }
+
+    public void switchToNoneMode()
+    {
+        mode = Mode.NONE;
+    }
+
+    public void switchToCreationMode()
+    {
+        mode = Mode.CREATION;
+    }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            // Check if the mouse was clicked over a UI element
-            if (!EventSystem.current.IsPointerOverGameObject())
+            Vector3 position = Input.mousePosition;
+            
+            if (mode==Mode.NONE)
             {
-                Vector3 position = Input.mousePosition;
-
-                position.z = zIndex;
-                Instantiate(testSphere, cam.ScreenToWorldPoint(position), Quaternion.identity);
+                bezierMenu.transform.SetPositionAndRotation(position, Quaternion.identity);
+                bezierMenu.SetActive(!bezierMenu.activeSelf);
             }
+            else if (mode == Mode.CREATION)
+            {
+                // Check if the mouse was clicked over a UI element
+                if (!EventSystem.current.IsPointerOverGameObject())
+                {
+                    
+
+                    Instantiate(testSphere, cam.ScreenToWorldPoint(position), Quaternion.identity);
+                }
+            }
+            
         }
         
     }

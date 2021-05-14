90e9a1c8c5a88ae26c41b65ffdd709bdb98b7a46
Sami Dalil <samee.essentials@gmail.com>
2021-05-14 14:09:04 +0200

    Refacto Polygon, BezierCurve et Point en accord avec les besoins pour l'interface

--- Assets/Scripts/Pixel Rendering/BezierCurve.cs
+++ Assets/Scripts/Pixel Rendering/BezierCurve.cs
@@ -1,95 +1,95 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
-public class BezierCurve : MonoBehaviour
+public class BezierCurve
 {
     #region Variables Unity
 
-    [SerializeField] private Polygon _controlPolygon;
-    [Range(1, 200)] [SerializeField] private int _step = 30;
+    public static int Step = 30;
+
+    public static List<BezierCurve> Elements = new List<BezierCurve>();
 
     #endregion
 
     #region Variables d'instance
 
-    private List<Vector3> _positions = new List<Vector3>();
+    public Polygon ControlPolygon;
+
+    public List<Vector3> Positions = new List<Vector3>();
 
     #endregion
 
-    #region Propriétés
-
-    public Polygon ControlPolygon
+    #region Constructeur
+    
+    public BezierCurve(Polygon controlPolygon)
     {
-        get => this._controlPolygon;
-        set
-        {
-            if (this._controlPolygon != null)
-            {
-                this._controlPolygon.Off("PositionChange", this.ComputeCurvePoints);
-                this._controlPolygon.Off("NewPoints", this.ComputeCurvePoints);
-            }
+        BezierCurve.Elements.Add(this);
+        this.ControlPolygon = controlPolygon;
+    }
 
-            this._controlPolygon = value;
-
-            if (this._controlPolygon != null)
-            {
-                this._controlPolygon.On("PositionChange", this.ComputeCurvePoints);
-                this._controlPolygon.On("NewPoints", this.ComputeCurvePoints);
-            }
-        }
+    ~BezierCurve()
+    {
+        BezierCurve.Elements.Remove(this);
     }
 
     #endregion
 
-    #region Foncitons Unity
+    #region Méthodes statiques
 
-    private void Update()
+    public static void DrawElements()
+    {
+        foreach (BezierCurve element in BezierCurve.Elements)
+            element.Draw();
+    }
+
+    #endregion
+
+    #region Méthodes publiques
+
+    public void Draws()
     {
         this.ComputeCurvePoints();
     }
 
-    private void OnDrawGizmos()
+    public void Draw()
     {
-        if (this._positions.Count > 1)
+        if (this.Positions.Count > 1)
         {
             Gizmos.color = Color.white;
 
-            for (int i = 0; i < this._positions.Count - 1; i++)
-                Gizmos.DrawLine(this._positions[i], this._positions[i + 1]);
+            for (int i = 0; i < this.Positions.Count - 1; i++)
+                Gizmos.DrawLine(this.Positions[i], this.Positions[i + 1]);
         }
     }
 
     #endregion
 
-    #region Fonctions privées
+    #region Méthodes privées
 
     private void ComputeCurvePoints()
     {
-        List<Point> points = this._controlPolygon.Points;
+        List<Point> points = this.ControlPolygon.Points;
         Vector3[,] arr = new Vector3[points.Count, points.Count];
-        this._positions.Clear();
+        this.Positions.Clear();
 
         for (int i = 0; i < points.Count; i++)
             arr[i, 0] = points[i].Position;
 
-        for (int n = 0; n <= this._step; n++)
+        for (int n = 0; n <= BezierCurve.Step; n++)
         {
-            float t = (float) n / this._step;
+            float t = (float) n / BezierCurve.Step;
 
             if (n == 0)
-                this._positions.Add(points[0].Position);
-            else if (n == this._step)
-                this._positions.Add(points[points.Count - 1].Position);
+                this.Positions.Add(points[0].Position);
+            else if (n == BezierCurve.Step)
+                this.Positions.Add(points[points.Count - 1].Position);
             else
             {
                 for (int j = 1; j < points.Count; j++)
                     for (int i = 0; i < points.Count - j; i++)
                        arr[i, j] = (1 - t) * arr[i, j - 1] + t * arr[i + 1, j - 1];
 
-                this._positions.Add(arr[0, points.Count - 1]);
+                this.Positions.Add(arr[0, points.Count - 1]);
             }
         }
     }

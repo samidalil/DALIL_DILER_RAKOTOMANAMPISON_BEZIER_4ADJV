e81db43ceca4a1974b89b5519ace40406b2588c6
Lior <diler.lior@gmail.com>
2021-05-14 18:58:11 +0200

    line renderer test

--- Assets/Scripts/Pixel Rendering/Point.cs
+++ Assets/Scripts/Pixel Rendering/Point.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class Point : MonoBehaviour
 {
-    #region Propriétés
+    #region PropriÃ©tÃ©s
 
     public Vector3 Position => this.transform.position;
-
+    
     #endregion
 }
